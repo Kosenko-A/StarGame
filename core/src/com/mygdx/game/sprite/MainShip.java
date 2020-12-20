db05ef65278a8d2d2a8d70ddfac1d77782457586
Kosenko <Kosenko.2000@yandex.ru>
2020-12-20 17:43:48 +0500

    add multitouch

--- core/src/com/mygdx/game/sprite/MainShip.java
+++ core/src/com/mygdx/game/sprite/MainShip.java
@@ -12,6 +12,8 @@ public class MainShip extends Sprite {
     private static final float HEIGHT = 0.15f;
     private static final float BUTTOM_MARGIN = 0.05f;
 
+    private static final int INVALID_POINTER = -1;
+
     private final Vector2 v;
     private final Vector2 v0;
 
@@ -19,6 +21,9 @@ public class MainShip extends Sprite {
 
     private boolean pressedLeft;
     private boolean pressedRight;
+
+    private int leftPointer = INVALID_POINTER;
+    private int rightPointer = INVALID_POINTER;
 
     public MainShip(TextureAtlas atlas) {
         super(atlas.findRegion("main_ship"), 1, 2, 2);
@@ -36,6 +41,14 @@ public class MainShip extends Sprite {
     @Override
     public void update(float delta) {
         pos.mulAdd(v, delta);
+        if (getRight() > worldBounds.getRight()){
+            setRight(worldBounds.getRight());
+            stop();
+        }
+        if (getLeft() < worldBounds.getLeft()){
+            setLeft(worldBounds.getLeft());
+            stop();
+        }
     }
 
     public boolean keyDown(int keycode) {
@@ -82,16 +95,38 @@ public class MainShip extends Sprite {
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
         if (touch.x < worldBounds.pos.x){
+            if (leftPointer != INVALID_POINTER){
+                return false;
+            }
+            leftPointer = pointer;
             moveLeft();
         } else {
+            if (rightPointer != INVALID_POINTER){
+                return false;
+            }
+            rightPointer = pointer;
             moveRight();
         }
-        return super.touchDown(touch, pointer, button);
+        return false;
     }
 
     @Override
     public boolean touchUp(Vector2 touch, int pointer, int button) {
-        stop();
+        if (pointer == leftPointer){
+            leftPointer = INVALID_POINTER;
+            if (rightPointer != INVALID_POINTER){
+                moveRight();
+            } else {
+                stop();
+            }
+        } else if (pointer == rightPointer){
+            rightPointer = INVALID_POINTER;
+            if (leftPointer != INVALID_POINTER){
+                moveLeft();
+            } else {
+                stop();
+            }
+        }
         return super.touchUp(touch, pointer, button);
     }
 
